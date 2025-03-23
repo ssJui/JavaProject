73fccb9adf869048ffeb46bc49551d86af70b09e
Sharmin Sultana Jui <ssjui.sust@gmail.com>
2025-03-23 10:51:35 +0600

    Added Background Colour

--- src/App.java
+++ src/App.java
@@ -9,11 +9,15 @@ public class App {
         int boardHeight = rowCount * tileSize;
 
         JFrame frame = new JFrame("Pac Man");
-        frame.setVisible(true);
+        //frame.setVisible(true);
         frame.setSize(boardWidth, boardHeight);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        PacMan pacmanGame = new PacMan();
+        frame.add(pacmanGame);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
---
+++ src/PacMan.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.HashSet;
+import java.util.Random;
+import javax.swing.*;
+
+public class PacMan extends JPanel {
+    private int rowCount = 21;
+    private int columnCount = 19;
+    private int tileSize = 32;
+    private int boardWidth = columnCount * tileSize;
+    private int boardHeight = rowCount * tileSize;
+
+    PacMan(){
+        setPreferredSize(new Dimension(boardWidth, boardHeight));
+        setBackground(Color.BLACK);
+    }
+}
